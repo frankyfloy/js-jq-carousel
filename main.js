4f819b0bec2165d0421ad56c6085eb013b049115
Francesco Sica <fsica171@gmail.com>
2021-03-11 17:20:08 +0100

    Press direzionale tastiera (codice da inserire in una funzione)

--- main.js
+++ main.js
@@ -12,6 +12,46 @@ function slider_Arrow(){
     var img = $("img");
     var circle = $(".fa-circle");
 
+    $("body").keydown(function(e) {
+        if(e.keyCode == 37) { // left
+            for (var i = 0; i < img.length; i++) {
+                if ($(img[i]).hasClass("active") && $(img[i]).hasClass("first")) {
+                    i = img.length;
+                } else if ($(img[i]).hasClass("active")) {
+
+                    $(img[i]).removeClass("active");
+                    $(img[i]).prev().addClass("active");
+
+                    $(circle[i]).removeClass("active");
+                    $(circle[i]).prev().addClass("active");
+
+                    i = img.length;
+                }
+                console.log(img[i]);
+            }
+
+        }else if(e.keyCode == 39) { // right
+            for (var i = 0; i < img.length; i++) {
+                if ($(img[i]).hasClass("active") && $(img[i]).hasClass("last")) {
+                    i = img.length;
+                }
+                else if ($(img[i]).hasClass("active")) {
+
+                    $(img[i]).removeClass("active");
+                    $(img[i]).next().addClass("active");
+
+                    $(circle[i]).removeClass("active");
+                    $(circle[i]).next().addClass("active");
+
+                    i = img.length;
+                }
+                console.log(img[i]);
+            }
+        }
+    });
+
+
+
     arrow_Right_Carousel.click( function () {
 
         for (var i = 0; i < img.length; i++) {
